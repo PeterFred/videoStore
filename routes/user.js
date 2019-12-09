67ddbb8f0d96ba2ead5a209ca3283f8a148011c5
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-12-09 14:19:45 +1300

    Added hashing to user password

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,9 @@ const { User, validate } = require("../models/user");
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const _ = require("lodash");
+
+const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
@@ -10,14 +13,13 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered.");
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  });
-
+  //Use lodash.pick to select props without creating new object
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
-  res.send(user);
+
+  res.send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
