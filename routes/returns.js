4c691c56a923260cec24d88a14acbf30b1fc603d
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-12-18 11:14:25 +1300

    TDD - / returns Rental is already returned

--- routes/returns.js
+++ routes/returns.js
@@ -13,7 +13,10 @@ router.post("/", async (req, res) => {
     "movie._id": req.body.movieId
   });
 
-  if (!rental) res.status(404).send("No customer found");
+  if (!rental) res.status(404).send("Rental not found");
+
+  if (rental.dateReturned)
+    return res.status(400).send("return already processed");
 
   res.status(401).send("Unauthorised");
 });
