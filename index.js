92a718dbfa619e71ea7a7f03c7afd869fcaeab77
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-12-18 15:39:52 +1300

    Add support for connection string via env variable

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation");
+// require("./startup/prod")(app);
 
-const port = process.env.port || 3000;
+const port = process.env.port || 5000;
 
 if (process.env.NODE_ENV !== "test") {
   app.listen(port);
+}
+
+if (process.env.NODE_ENV !== "production") {
+  require("./startup/prod")(app);
 }
 
 const server = app.listen(port, () =>
